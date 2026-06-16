bbd7bfcc8d70e73071e8ea241fa464f4e219e6ca
Gokac9 <theabraham9@gmail.com>
2026-06-16 09:37:50 +0200

    fix quantity

--- msc_fsm/models/fsm_order.py
+++ msc_fsm/models/fsm_order.py
@@ -384,7 +384,7 @@ class DaruclimeFSMOrder(models.Model):
                         "product_id": mat.product_id.id,
                         "name": mat.name,
                         "product_uom_qty": mat.quantity,
-                        # "product_uom": mat.uom_id.id,
+                        "product_uom_id": mat.uom_id.id,
                     },
                 )
             )
@@ -400,7 +400,7 @@ class DaruclimeFSMOrder(models.Model):
                             "product_id": service_product.id,
                             "name": f"{ts.employee_id.name}: {ts.name or service_product.display_name}",
                             "product_uom_qty": ts.duration,
-                            "product_uom": service_product.uom_id.id,
+                            "product_uom_id": service_product.uom_id.id,
                         },
                     )
                 )
